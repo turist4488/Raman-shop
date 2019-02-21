2002a493c0e0f6959f1c24b8ce0923f7493e610b
turist4488 <turist4488@gmail.com>
2019-02-21 21:43:53 +0200

    prettier was injected

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Details extends Component {
-    render() {
-        return (
-            <div>
-                <h2>I am Details</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>I am Details</h2>
+      </div>
+    );
+  }
 }
 
 export default Details;
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -5,35 +5,35 @@ import products from './phones/phones';
 
 class ProductList extends Component {
 
-    render() {
+  render() {
 
-        return (
-            <div className='container-fluid'>
-                <div className="row">
-                    <div className="col-md-2">
-                        <p>Search: <input type="text"/></p>
-                        <p>Sort by:
-                            <select name="" id="">
-                                <option value="name">Alphabetical</option>
-                                <option value="age">Newest</option>
-                            </select>
-                        </p>
-                    </div>
-                    <div className='col-md-10'>
-                        <ul className='phones'>
-                            {products.map(product => (
-                                <li className='thumbnail'>
-                                    <Link className='thumb' to="/product"><img src={product.imageUrl} alt=""/></Link>
-                                    <Link to='/product'>{product.name}</Link>
-                                    <p>{product.snippet}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className='container-fluid'>
+        <div className="row">
+          <div className="col-md-2">
+            <p>Search: <input type="text"/></p>
+            <p>Sort by:
+              <select name="" id="">
+                <option value="name">Alphabetical</option>
+                <option value="age">Newest</option>
+              </select>
+            </p>
+          </div>
+          <div className='col-md-10'>
+            <ul className='phones'>
+              {products.map(product => (
+                <li className='thumbnail'>
+                  <Link className='thumb' to="/product"><img src={product.imageUrl} alt=""/></Link>
+                  <Link to='/product'>{product.name}</Link>
+                  <p>{product.snippet}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProductList;
