6c50723487e1d565082e7494fec3e730ec418453
turist4488 <turist4488@gmail.com>
2019-03-06 15:48:10 +0200

    Updates

---
+++ precache-manifest.56f0dcce4ff315d15f1fbf1c47e19071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f4e7bb58efabbfb44dc",
+    "url": "/Raman-shop/static/js/runtime~main.6f4e7bb5.js"
+  },
+  {
+    "revision": "a6930ac52689385c052c",
+    "url": "/Raman-shop/static/js/main.a6930ac5.chunk.js"
+  },
+  {
+    "revision": "dc44dbe67d01e2df3f58",
+    "url": "/Raman-shop/static/js/2.dc44dbe6.chunk.js"
+  },
+  {
+    "revision": "a6930ac52689385c052c",
+    "url": "/Raman-shop/static/css/main.8c9c1a3f.chunk.css"
+  },
+  {
+    "revision": "dc44dbe67d01e2df3f58",
+    "url": "/Raman-shop/static/css/2.8f940351.chunk.css"
+  },
+  {
+    "revision": "b37b9c4a095565e8b295d7e8fe65211e",
+    "url": "/Raman-shop/index.html"
+  }
+];
