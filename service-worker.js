1e039d99bd1368fbdf346a840ebaced23e30ff02
turist4488 <turist4488@gmail.com>
2019-03-06 20:13:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Raman-shop/precache-manifest.61cc874a27e9522c4eab422e19a89dba.js"
+  "/store/precache-manifest.f3f4d0c27e9f63a342e981d768f8c55b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Raman-shop/index.html", {
+workbox.routing.registerNavigationRoute("/store/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
